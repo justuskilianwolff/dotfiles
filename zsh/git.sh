8a9897ccaa8a833c2244a9555772c27faf126d6e
Justus <justus.wolff@audit.vision>
2024-09-10 08:36:06 +0200

    added some git aliases

---
+++ zsh/git.sh
@@ -0,0 +1,15 @@
+# basic aliases
+alias gs='git status'
+alias ga='git add'
+alias gb='git branch'
+alias gm='git merge'
+alias gd='git diff'
+alias gl='git log'
+alias gcm='git commit'
+alias gco='git checkout'
+alias gp='git pull'
+alias gps='git push'
+
+# convenienve aliases
+alias gundo='git reset --soft HEAD~1' # Undo last commit
+alias gdelloc='git branch --merged | grep -v "\*" | grep -v "main" | grep -v "master" | xargs -n 1 git branch -d' # Delete all local branches that have been merged
