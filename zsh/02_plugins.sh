2f5b18125104300a77e7a6ff0f71c86fdce8e0b2
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-12 21:08:58 +0200

    added plugins

---
+++ zsh/02_plugins.sh
@@ -0,0 +1,5 @@
+source $HOMEBREW_PREFIX/share/powerlevel10k/powerlevel10k.zsh-theme
+source $HOMEBREW_PREFIX/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $HOMEBREW_PREFIX/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source $HOMEBREW_PREFIX/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
