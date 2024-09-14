02b57372fb2d322d1c2fd9ade1c60a2995c41b54
Justus Kilian Wolff <justus.wolff@web.de>
2024-09-14 10:32:27 +0200

    fixing issue with tab and using most fitting file path

--- zsh/04_navigation.sh
+++ zsh/04_navigation.sh
@@ -17,15 +17,3 @@ alias reps="cd ~/Repositories"
 # misc
 alias h='history'
 alias c='clear'
-
-
-function forward-word-begin(){
-    zle forward-word
-    zle beginning-of-word
-}
-zle -N forward-word-begin
-
-
-# key bindings
-bindkey '^I' autosuggest-accept
-# accept the next word with alt+right (especially useful for autosuggestions of paths)
