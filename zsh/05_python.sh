7ffb22888d42fe96bb7cf40744b13a81e84aa2a5
Justus <justus.wolff@audit.vision>
2024-09-16 08:04:17 +0200

    use rye format and lint instead of ruff

--- zsh/05_python.sh
+++ zsh/05_python.sh
@@ -1,15 +1,14 @@
-function ru(){
-    # format the code using ruff
-    ruff format 
-    # check and fix the code using ruff
-    ruff check --fix
-}
-
 # basic aliases for the rye package manager in python
 alias rs='rye sync'
 alias ri='rye init'
 alias ra='rye add'
 alias rr='rye remove'
+function rf(){
+    # format the code using ruff
+    rye fmt
+    # lint and fix the code using ruff
+    rye lint --fix
+}
 
 # django aliases
 alias dj='python manage.py'
