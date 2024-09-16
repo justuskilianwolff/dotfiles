e75d3a55edf70a96b72bd2951914483c6b77f55d
Justus <justus.wolff@audit.vision>
2024-09-16 13:38:40 +0200

    disabled oh my zsh for now

--- zsh/01_oh-my-zsh.sh
+++ zsh/01_oh-my-zsh.sh
@@ -70,9 +70,9 @@ export ZSH="$HOME/.oh-my-zsh"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+# plugins=(git)
 
-source $ZSH/oh-my-zsh.sh
+# source $ZSH/oh-my-zsh.sh
 
 # User configuration
 
