d497170a05ce3dc289fbd631edd1457ca35c240a
Justus <justus.wolff@audit.vision>
2024-09-16 13:38:50 +0200

    improved git shortcuts

--- zsh/03_git.sh
+++ zsh/03_git.sh
@@ -1,4 +1,5 @@
 # basic aliases
+alias g='git'
 alias gs='git status'
 alias ga='git add'
 alias gb='git branch'
@@ -6,10 +7,18 @@ alias gm='git merge'
 alias gd='git diff'
 alias gl='git log'
 alias gcm='git commit'
-alias gc='git checkout'
 alias gp='git pull'
 alias gh='git push'
 
 # convenienve aliases
 alias gundo='git reset --soft HEAD~1' # Undo last commit
 alias gdelloc='git branch --merged | grep -v "\*" | grep -v "main" | grep -v "master" | xargs -n 1 git branch -d' # Delete all local branches that have been merged
+
+# git checkout function
+function gc(){
+    if [ $# -eq 0 ]; then
+        git checkout main
+    else
+        git checkout "$@"
+    fi
+}
