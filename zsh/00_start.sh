0fe4458fc61b6a5e488da590c1bbb09698884888
Justus <justus.wolff@audit.vision>
2024-12-11 11:32:03 +0100

    added todo

--- zsh/00_start.sh
+++ zsh/00_start.sh
@@ -1,1 +1,6 @@
 export STARSHIP_CONFIG="$DOTFILES_PATH/starship.toml"
+
+# try to get this working
+# https://stackoverflow.com/questions/10406926/how-do-i-change-the-default-virtualenv-prompt
+# https://stackoverflow.com/questions/78615121/where-is-the-setting-to-remove-the-name-of-the-venv-from-vs-codes-terminal-prom
+# export VIRTUAL_ENV_DISABLE_PROMPT=false # remove the virtualenv prompt from the shell (name of env was printed)
