2aa6f2901b8ca9da227b73db58dc5bd700561093
Justus <justus.wolff@audit.vision>
2025-01-21 10:38:11 +0100

    added rust aliases

---
+++ zsh/06_rust.sh
@@ -0,0 +1,10 @@
+# source cargo 
+source $HOME/.cargo/env
+
+# add some shortcuts
+alias ci="cargo init"
+alias cn="cargo new"
+alias cb="cargo build"
+alias cbr="cargo build --release"
+alias cr="cargo run"
+alias cc="cargo check"
