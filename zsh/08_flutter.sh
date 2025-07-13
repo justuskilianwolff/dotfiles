dc3ed9fd6b8974ff9d507016cc283eab8660ae94
justuskilianwolff <justus.wolff@web.de>
2025-07-13 14:32:33 +0200

    flutter

---
+++ zsh/08_flutter.sh
@@ -0,0 +1,6 @@
+alias fl="fvm flutter"
+alias da="fvm dart"
+
+# FVM and flutter
+export PATH="$HOME/.fvm_flutter/bin:$PATH"
+export PATH="$PATH:$HOME/.fvm/bin" # might not be needed anymore?!
--- zsh/98_misc.sh
+++ zsh/98_misc.sh
@@ -1,4 +1,6 @@
+
+# Homebrew aliases
 alias bu="brew upgrade && brew cu -ayf && brew cleanup"
 
-# fvm via dart: dart pub global activate fvm
-export PATH="$PATH":"$HOME/.pub-cache/bin"
+# Miscellaneous aliases
+alias ez="exec zsh"
