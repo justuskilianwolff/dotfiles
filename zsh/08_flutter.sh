4cd279d19e0946d44a6add797f6dc584baad2e0a
Justus <justus.wolff@audit.vision>
2025-07-14 09:10:40 +0200

    fixed flutter path

--- zsh/08_flutter.sh
+++ zsh/08_flutter.sh
@@ -1,11 +1,20 @@
 # FVM and flutter
 export PATH="$HOME/.fvm_flutter/bin:$PATH"
-#export PATH="$PATH:$HOME/.fvm/bin" # TODO: might not be needed anymore
+export PATH="$PATH:$HOME/fvm/default/bin" # links to the global flutter version of fvm, only use outside of project
 
 # aliases for fvm
 # install via curl -fsSL https://fvm.app/install.sh | bash (https://fvm.app/documentation/getting-started/installation)
 alias fl="fvm flutter"
 alias da="fvm dart"
+
+# add some aliases for fvm commands
+alias fu="fvm use"
+alias fin="fvm install"
+alias fls="fvm list"
+alias fr="fvm releases"
+alias frm="fvm remove"
+alias fg="fvm global"
+alias fdo="fvm doctor"
 
 # add some aliases for flutter commands
 alias flr="fl run"
