fe270a9cdc27f56c351977c3296d738eefab0908
Justus Kilian Wolff <wolff@akeno.ai>
2025-10-15 14:28:52 +0200

    feat: added fzf

--- zsh/99_end.sh
+++ zsh/99_end.sh
@@ -1,3 +1,5 @@
 eval "$(starship init zsh)" # run as last to ensure all other configurations are loaded first
 
 eval "$(zoxide init zsh)" # zoxide init
+
+source <(fzf --zsh) # added fuzzy find (activated with Ctrl-R)
